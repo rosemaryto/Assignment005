ecc3e6b36c07cb236bfe5dd4b708de76be76fcac
rosemaryto <rosemary_to@ymail.com>
2022-03-22 19:14:32 -0700

    again

--- beginning/js/script.js
+++ beginning/js/script.js
@@ -1,31 +1,17 @@
 const $ = (id) => document.getElementById(id);
-
 let id = $("id");
-
 let name = $("name");
-
 let ext = $("ext");
-
 let email = $("email");
-
 let department = $("department");
-
 let empForm = $("empForm");
 
  
-
 empForm.addEventListener('submit', (e) => {
-
   e.preventDefault();
-
   console.log(`ID: ${id.value}`);
-
   console.log(`Name: ${name.value}`);
-
   console.log(`Extension: ${ext.value}`);
-
   console.log(`Email: ${email.value}`);
-
   console.log(`Department: ${department.value}`);
-
 })
